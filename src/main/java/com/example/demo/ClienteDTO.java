2cf8aa370db0c4463428f751b1c6f385f2e2e478
Joao <joaopedrocorrea160@gmail.com>
2022-10-25 20:58:46 -0300

    Adicionando ClienteDTO

---
+++ src/main/java/com/example/demo/ClienteDTO.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class ClienteDTO {
+    private String nome;
+    private String endereco;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
