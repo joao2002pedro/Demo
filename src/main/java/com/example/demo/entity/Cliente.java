4cddcf34027fc1f4000b4988afe4fb4848d56cdf
unknown <joaopedrocorrea160@gmail.com>
2022-10-31 20:10:05 -0300

    Alguma coisa mudei

--- src/main/java/com/example/demo/entity/Cliente.java
+++ src/main/java/com/example/demo/entity/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Entity
 public class Cliente
@@ -13,32 +14,21 @@ public class Cliente
     private Long id;
     private String nome;
     private String endereco;
+    private List<Produto> produtos;
 
 
-    public Cliente()
-    {
-    }
-
-    public Cliente(Long id, String nome, String endereco) {
+    public Cliente(Long id, String nome, String endereco, List<Produto> produtos) {
         this.id = id;
         this.nome = nome;
         this.endereco = endereco;
+        this.produtos = produtos;
     }
 
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+    public Cliente() {
     }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getNome() {
@@ -49,4 +39,19 @@ public class Cliente
         this.nome = nome;
     }
 
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
 }
